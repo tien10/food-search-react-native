878f61c5844610b77c0a3260462ada27eea4ae2d
tien10 <nguyentien7566@gmail.com>
2020-10-23 23:56:08 +0700

    1 dong bug

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const SearchScreen = () => {
+    return <View>
+        <Text>SearchScreen</Text>
+    </View>
+}
+
+export default SearchScreen
